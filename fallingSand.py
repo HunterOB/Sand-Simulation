9c78d41d8b76ca82621f5c75fe3a51835ce59cbd
HunterOB <hunterobrien321@gmail.com>
2025-04-03 18:06:25 +1300

    Create fallingSand.py
    main file

---
+++ fallingSand.py
@@ -0,0 +1,24 @@
+import time
+import pygame 
+
+
+def main():
+    # Colours for pygame
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+
+    pygame.init()
+    screen_size = (800, 800)
+
+    screen = pygame.display.set_mode(screen_size)
+    pygame.display.set_caption("Sand Gravity Simulation")
+
+    # CLock for FPS
+
+
+
+
+
+def new_grid():
+    grid = [[],[],[],[]]
